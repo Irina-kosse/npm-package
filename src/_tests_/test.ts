4872406bc6156c763b0778010c66aef8e9e8716e
Irina-kosse <iryna.rysenko@gmail.com>
2022-06-13 22:23:13 +0300

    patch

--- src/_tests_/test.ts
+++ src/_tests_/test.ts
@@ -1,25 +1,19 @@
-import { quadraticEquation } from "../quadratic"
+import { quadraticEquation } from "../quadratic";
 
-test('should have array of numbers', () => {
-    expect(quadraticEquation(1, 6, 5)).toEqual(
-      expect.arrayContaining([expect.any(Number)])
-    );
+test("should have array of numbers", () => {
+  expect(quadraticEquation(1, 6, 5)).toEqual(
+    expect.arrayContaining([expect.any(Number)])
+  );
 });
 
-test('should return array of roots if discriminant > 0', () => {
-    expect(quadraticEquation(1, 6, 5)).toEqual(
-      expect.arrayContaining([-1, -5])
-    );
+test("should return array of roots if discriminant > 0", () => {
+  expect(quadraticEquation(1, 6, 5)).toEqual(expect.arrayContaining([-1, -5]));
 });
 
-test('should return array of equal roots', () => {
-    expect(quadraticEquation(1, -6, 9)).toEqual(
-      expect.arrayContaining([3, 3])
-    );
+test("should return array of equal roots", () => {
+  expect(quadraticEquation(1, -6, 9)).toEqual(expect.arrayContaining([3, 3]));
 });
 
-test('should return array of real and imaginary root', () => {
-    expect(quadraticEquation(1, -6, 9)).toEqual(
-      expect.arrayContaining([3, 3])
-    );
+test("should return array of real and imaginary root", () => {
+  expect(quadraticEquation(1, -6, 9)).toEqual(expect.arrayContaining([3, 3]));
 });
