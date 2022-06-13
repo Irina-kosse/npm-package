2950e00e826b43e48c40750116446722a7cf4aad
Irina-kosse <iryna.rysenko@gmail.com>
2022-06-13 22:33:34 +0300

    1.0.5

--- src/quadratic.ts
+++ src/quadratic.ts
@@ -1,33 +1,32 @@
 // program to solve quadratic equation
-export const quadraticEquation = (a : number, b : number, c : number)  => {
+export const quadraticEquation = (a: number, b: number, c: number) => {
+  const result = [];
+  let root1;
+  let root2;
+  // calculate discriminant
+  const discriminant = b * b - 4 * a * c;
 
-const result = [];
-let root1; 
-let root2;
-// calculate discriminant
-const discriminant = b * b - 4 * a * c;
-
-// condition for real and different roots
-if (discriminant > 0) {
+  // condition for real and different roots
+  if (discriminant > 0) {
     root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
     root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
-    result.push( root1, root2);
-}
+    result.push(root1, root2);
+  }
 
-// condition for real and equal roots
-else if (discriminant === 0) {
+  // condition for real and equal roots
+  else if (discriminant === 0) {
     root1 = root2 = -b / (2 * a);
 
     result.push(root1, root2);
-}
+  }
 
-// if roots are not real
-else {
-    const realPart : number = parseFloat((-b / (2 * a)).toFixed(2));
-    const imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2) + 'i';
+  // if roots are not real
+  else {
+    const realPart: number = parseFloat((-b / (2 * a)).toFixed(2));
+    const imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2) + "i";
 
     result.push(realPart, imagPart);
-}
-    return result;
-}
+  }
+  return result;
+};
